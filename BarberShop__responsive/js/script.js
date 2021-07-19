f54c90116dfbfa2c3155f59d1ebd76f18162bb04
Gor Darbinyan <darbinyangor1@gmail.com>
2021-07-20 00:50:31 +0400

    Fixt desktop responsivre

--- BarberShop__responsive/js/script.js
+++ BarberShop__responsive/js/script.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     items: 1,
     loop: true,
-
+    responsiveClass: true,
+    responsive: {
+      1200: {
+        items: 3,
+        loop: false,
+        mouseDrag: false,
+        touchDrag: false
+      },
+    }
   });
 });
